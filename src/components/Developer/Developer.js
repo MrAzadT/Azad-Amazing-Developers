be84efca679b0e4ae7cae1c300633a158c6079f9
azadtamal <mrazadt@gmail.com>
2021-09-26 16:23:48 +0600

    50%

--- src/components/Developer/Developer.js
+++ src/components/Developer/Developer.js
@@ -1,14 +1,21 @@
 import React from "react";
+import "./Developer.css";
 
 const Developer = (props) => {
   const { name, skills, country, salary, img } = props.data;
   return (
-    <div>
-      <img src={img} alt="" />
-      <h1>name: {name}</h1>
-      <p>country : {country}</p>
-      <p>salary : {salary}</p>
-      <p>skills : {skills}</p>
+    <div className="card">
+      <div className="text-center">
+        <img className="mt-3" src={img} alt="" />
+        <h4 className="mt-3">Name: {name}</h4>
+        <p>Country : {country}</p>
+        <p>Salary : {salary}</p>
+        <p>Skills : {skills}</p>
+        <button type="button" className="btn btn-primary mb-2">
+          <box-icon name="heart" type="solid"></box-icon>
+          add favourite
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,20 +1,24 @@
 import React, { useState } from "react";
-
+import "./Main.css";
 import myData from "../../myData/myData";
 import Developer from "../Developer/Developer";
+import Favourite from "./../favourite/Favourite";
 
 const Main = () => {
   const [data, setData] = useState(myData);
   const [cart, setCart] = useState([]);
-  console.log(data);
+
+  const handleBtn = () => {};
   return (
     <div className="main">
       <div className="developer">
         {data.map((data) => (
-          <Developer data={data} />
+          <Developer data={data} handleBtn={handleBtn} />
         ))}
       </div>
-      <div className="cart"></div>
+      <div className="favourite">
+        <Favourite></Favourite>
+      </div>
     </div>
   );
 };
